ec0b2c6af81e22b36bc69e39a7728231d17ae74f
mithun139-WAL <96226840+mithun139-WAL@users.noreply.github.com>
2022-04-01 11:55:29 +0530

    added files

--- node/routes/exam.js
+++ node/routes/exam.js
@@ -7,7 +7,7 @@ let salt = bcrypt.genSaltSync(10);
 
 router.get('/createtable', function (req, res) {
   connector.query(
-    'CREATE TABLE usertable (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(100));',
+    'CREATE TABLE usertable (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL, password VARCHAR(100) NOT NULL);',
     function (err, results) {
       res.json({err, results});
     }
